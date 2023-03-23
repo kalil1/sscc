3d4322234bd5ae64129d47b04b1745b5c7dcf8f6
kalil1 <wkalil77@gmail.com>
2023-03-23 12:42:26 -0400

    chore: test model validations and controller actions

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    let(:product) { build(:product) }
+
+    it "validates numericality of length, width, height, and weight" do
+      product.length = "not a number"
+      product.width = "not a number"
+      product.height = "not a number"
+      product.weight = "not a number"
+      expect(product).not_to be_valid
+
+      product.length = -1
+      product.width = -1
+      product.height = -1
+      product.weight = -1
+      expect(product).not_to be_valid
+
+      product.length = 0
+      product.width = 0
+      product.height = 0
+      product.weight = 0
+      expect(product).not_to be_valid
+
+      product.length = 1
+      product.width = 1
+      product.height = 1
+      product.weight = 1
+      expect(product).to be_valid
+    end
+  end
 end
--- spec/requests/api/v1/products_spec.rb
+++ spec/requests/api/v1/products_spec.rb
@@ -1,7 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Products", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET #index" do
+    it "returns http success" do
+      get '/api/v1/products'
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #show" do
+    it "returns http success" do
+      product = FactoryBot.create(:product)
+      get '/api/v1/products/' + product.id
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #edit" do
+    it "returns http success" do
+      product = FactoryBot.create(:product)
+      get "/api/v1/products/?id=#{product.id}/edit"
+      expect(response).to have_http_status(:success)
+    end
   end
 end
